b8cea7ca00e24a3ceb404810779c2963060f72cb
sukalov <matvey10@gmail.com>
2024-12-12 18:56:08 -0700

    schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,17 +1,17 @@
 import { Category } from "@/types/types";
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { title } from "process";
 
 export const songbook = sqliteTable("songbook", {
   id: text("id").primaryKey(),
   category: text("category").notNull().$type<Category>(),
-  artist: text("artist"),
   title: text("title").notNull(),
+  artist: text("artist"),
   artist_name: text("artist_name"),
   link: text("link").notNull(),
   additional_chords: int("additional_chords"),
 });
+// id,category,artist,artist_nime,title,link,additional_chords
 
 export type SongbookSelect = InferSelectModel<typeof songbook>;
 export type SongbookInsert = InferInsertModel<typeof songbook>;
