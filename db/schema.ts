978b9ffe358824a7982c41062294865674e407b8
sukalov <matvey10@gmail.com>
2024-12-17 16:40:41 -0700

    fix: typo in schema

--- db/schema.ts
+++ db/schema.ts
@@ -17,13 +17,12 @@ export const songbook = sqliteTable("songbook", {
 
 // id, chat_id, username, tg_name, saved_name, added_at
 export const users = sqliteTable("users", {
-  id: int("id").primaryKey(),
-  chat_id: int("chat_id").notNull(),
+  chat_id: int("chat_id").primaryKey(),
   username: text("username"),
   tg_name: text("tg_name"),
   saved_name: text("saved_name"),
   added_at: int("added_at", { mode: "timestamp" }),
-  times_performed: int("times_perofrmed").$default(() => 0),
+  times_performed: int("times_performed").$default(() => 0),
 });
 
 export type SongbookSelect = InferSelectModel<typeof songbook>;
