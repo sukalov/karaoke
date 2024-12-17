82b7cc8f18b62c9c968d30583492395464201c08
sukalov <matvey10@gmail.com>
2024-12-17 13:46:47 -0700

    feat: update schema. implement not showing excluded songs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { db } from "@/db";
 import SongChooser from "./song-catalogue";
 import { songbook } from "@/db/schema";
-import { sql } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 
 export default async function HomePage() {
-  const query = db.select().from(songbook).orderBy(sql`${songbook.artist} nulls last`)
-  const songs = await query
+  const query = db
+    .select()
+    .from(songbook)
+    .where(eq(songbook.excluded, false))
+    .orderBy(sql`${songbook.artist} nulls last`);
+  const songs = await query;
   return <SongChooser songs={songs} />;
 }
--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,7 @@ import { Category } from "@/types/types";
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+// id,category,artist,artist_nime,title,link,additional_chords
 export const songbook = sqliteTable("songbook", {
   id: text("id").primaryKey(),
   category: text("category").notNull().$type<Category>(),
@@ -10,8 +11,22 @@ export const songbook = sqliteTable("songbook", {
   artist_name: text("artist_name"),
   link: text("link").notNull(),
   additional_chords: int("additional_chords"),
+  excluded: int("excluded", { mode: "boolean" }).$default(() => false),
+  counter: int("counter").$default(() => 0), // times this song was selected
 });
-// id,category,artist,artist_nime,title,link,additional_chords
+
+// id, chat_id, username, tg_name, saved_name, added_at
+export const users = sqliteTable("users", {
+  id: int("id").primaryKey(),
+  chat_id: int("chat_id").notNull(),
+  username: text("username"),
+  tg_name: text("tg_name"),
+  saved_name: text("saved_name"),
+  added_at: int("added_at", { mode: "timestamp" }),
+  times_performed: int("times_perofrmed").$default(() => 0),
+});
 
 export type SongbookSelect = InferSelectModel<typeof songbook>;
 export type SongbookInsert = InferInsertModel<typeof songbook>;
+export type UsersSelect = InferSelectModel<typeof users>;
+export type UsersInsert = InferInsertModel<typeof users>;
