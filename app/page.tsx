e0b867d4d5692aeed96768d74cfb29ad88f465f7
sukalov <matvey10@gmail.com>
2025-01-25 05:48:31 +0300

    order by random

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { db } from "@/db";
-import SongChooser from "./song-catalogue";
 import { songbook } from "@/db/schema";
 import { eq, sql } from "drizzle-orm";
+
+import SongChooser from "./song-catalogue";
 
 export default async function HomePage() {
   const query = db
     .select()
     .from(songbook)
     .where(eq(songbook.excluded, false))
-    .orderBy(sql`${songbook.artist} nulls last`);
+    .orderBy(sql`random()`);
+  // .orderBy(sql`${songbook.artist} nulls last`);
   const songs = await query;
   return <SongChooser songs={songs} />;
 }
