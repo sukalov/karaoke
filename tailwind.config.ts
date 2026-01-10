660affc20e413877b7f8d0dc31aec229f8210e70
sukalov <matvey10@gmail.com>
2026-01-10 08:12:44 +0300

    chore: update deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 const plugin = require("tailwindcss/plugin");
 
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
